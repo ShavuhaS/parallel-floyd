92f2259b422dbc57d8f9f0a7069cabf2542ea856
Andrii <andreyhohotva2@gmail.com>
2026-04-03 17:30:59 +0300

    removed comments from tests

--- internal/floyd/shared_test.go
+++ internal/floyd/shared_test.go
@@ -16,8 +16,6 @@ import (
 const TEST_INPUT_DIR = "testdata/input"
 const TEST_DIST_DIR = "testdata/dist"
 
-// const TEST_PREV_DIR = "testdata/prev"
-
 func fileTestFloyd(t *testing.T, floyd func([][]float64)) {
 	testInputs, err := os.ReadDir(TEST_INPUT_DIR)
 	if err != nil {
@@ -69,7 +67,6 @@ func fileTestFloydWithPath(t *testing.T, floyd func([][]float64, [][]int)) {
 		t.Run(baseName, func(t *testing.T) {
 			inputPath := filepath.Join(TEST_INPUT_DIR, baseName+".input.txt")
 			distPath := filepath.Join(TEST_DIST_DIR, baseName+".dist.txt")
-			// prevPath := filepath.Join(TEST_PREV_DIR, baseName+".prev.txt")
 
 			inputMat, err := utils.InputFromFile(inputPath)
 			if err != nil {
@@ -81,17 +78,11 @@ func fileTestFloydWithPath(t *testing.T, floyd func([][]float64, [][]int)) {
 				t.Fatalf("Unable to read test output file (dist): %v", err)
 			}
 
-			// expectedPrev, err := utils.PrevFromFile(prevPath)
-			// if err != nil {
-			// 	t.Fatalf("Unable to read test output file (prev): %v", err)
-			// }
-
 			dist := InitDist(inputMat)
 			prev := InitPrev(inputMat)
 			floyd(dist, prev)
 
 			utils.AssertMatricesEqual(t, dist, expectedDist, INF)
-			// utils.AssertMatricesEqual(t, prev, expectedPrev, math.MaxInt)
 			utils.AssertFloydDistMatchesPrev(t, inputMat, dist, prev)
 		})
 	}
